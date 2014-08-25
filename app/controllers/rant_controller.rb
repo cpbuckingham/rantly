ddd4de948db62b7196d98e5337305e52fdb0a6c0
Cameron <cameron.p.buckingham@gmail.com>
2014-08-25 16:39:29 -0600

    cleaned it up

--- app/controllers/rant_controller.rb
+++ app/controllers/rant_controller.rb
@@ -1,45 +1,48 @@
 class RantController < ApplicationController
+  def new
+    @rant = Rant.new
+  end
 
   def index
     @rant = Rant.order(:title)
   end
 
-  def edit
-    @rant = Rant.new
-  end
-
   def create
-    @rant = Rant.create(title: params[:rants][:title])
-
-    if @rant.valid?
+    @rant = Rant.new(title: params[:rant][:title],
+                     content: params[:rant][:content],
+                      rant_id: params[:rant][:rant_id])
+    if @rant.save
       flash[:notice] = "Rant created successfully!"
-      redirect_to root_path
+      redirect_to rant_index_path
     else
       @rant.errors
       render :edit
     end
   end
 
-  def new
+  def edit
     @rant = Rant.find(params[:id])
   end
 
   def update
     @rant = Rant.find(params[:id])
     if @rant.update(
-      title: params[:rants][:title],
-      content: params[:rants][:content])
+      title: params[:rant][:title],
+      content: params[:rant][:content])
       flash[:notice] = "Your rant was successfully updated!"
-      redirect_to root_path
+      redirect_to rant_index_path
     else
-      render :new
+      render :edit
     end
   end
+
   def destroy
     @rant = Rant.find(params[:id])
     @rant.destroy
-    redirect_to root_path
+    flash[:notice] = "Your rant was deleted!"
+    redirect_to rant_index_path
   end
+
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "root#index"
   resources :registrations, only: [:new, :create, :edit, :update]
   resource :sessions, only: [:new, :create, :destroy]
-  resources :rant, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :rant
 
   get "signin" => "sessions#new"
   post "signin" => "sessions#create"
