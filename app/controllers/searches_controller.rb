bf1b318ae0055145f7090e65ed080dd967870dd0
Cameron <cameron.p.buckingham@gmail.com>
2014-09-30 14:06:41 -0600

    searching title and content on rants

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,6 @@ class SearchesController < ApplicationController
     def index
       params[:search] ||= "alskdfjlakjsdfihoqw}}KQEPifj"
       @rants = Rant.joins(:user).where('username LIKE ? OR first_name LIKE ? OR last_name LIKE ?', params[:search]+"%",params[:search]+"%",params[:search]+"%")
+      @rant = Rant.where('title LIKE ? OR content LIKE ?', params[:search]+"%",params[:search]+"%")
     end
 end
