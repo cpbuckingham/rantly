0c13e2da1236be500341a6a33910076497f05710
Cameron <cameron.p.buckingham@gmail.com>
2014-10-01 10:01:02 -0600

    gem for markdown translator installed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,8 @@ class SearchesController < ApplicationController
     def index
       params[:search] ||= "alskdfjlakjsdfihoqw}}KQEPifj"
       @rants = Rant.joins(:user).where('username LIKE ? OR first_name LIKE ? OR last_name LIKE ?', params[:search]+"%",params[:search]+"%",params[:search]+"%")
-      @rant = Rant.where('title LIKE ? OR content LIKE ?', params[:search]+"%",params[:search]+"%")
+      @rant = Rant.where('title LIKE ?', params[:search]+"%")
+      @contents = Rant.where('content LIKE ?', params[:search]+"%")
       @mentioned = User.where('content LIKE ?', params[:search]+"@%")
     end
 end
