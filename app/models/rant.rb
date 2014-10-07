513f9c4fa32afdd691f37996dc7dfcc80b15efba
Cameron <cameron.p.buckingham@gmail.com>
2014-10-07 10:24:13 -0600

    validates length of rants, both title and content, no tests

--- app/models/rant.rb
+++ app/models/rant.rb
@@ -2,4 +2,6 @@ class Rant < ActiveRecord::Base
 belongs_to :user
 has_many :favorites
   validates :title, :content, presence: true
+  validates_length_of :content, :minimum => 140
+  validates_length_of :title, :maximum => 50
 end
